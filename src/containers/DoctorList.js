c3142d7fa550345e3760e6dc4ee7f1361dff6b63
mguevara717 <marissa.guevara717@gmail.com>
2018-11-25 14:14:35 -0500

    fixed bug

--- src/containers/DoctorList.js
+++ src/containers/DoctorList.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import DoctorCard from '../components/DoctorCard'
 
-const DoctorList = () => {
+const DoctorList = (props) => {
+
+  console.log(props)
+
 
   return(
     <div>
-      
     </div>
   )
 }
--- src/containers/DoctorPage.js
+++ src/containers/DoctorPage.js
@@ -7,23 +7,22 @@ class DoctorPage extends React.Component {
   };
 
   componentDidMount() {
-    fetch(
-      "https://api.betterdoctor.com/2016-03-01/practices?name=therapist&skip=0&limit=100&user_key=21802ea0d6d2c565488582088e578a6c")
+    fetch("https://api.betterdoctor.com/2016-03-01/practices?name=therapist&skip=0&limit=100&user_key=21802ea0d6d2c565488582088e578a6c")
       .then(response => response.json())
       .then(doctors => {
+        const docList = doctors.data
         this.setState({
-          doctors: doctors
+          doctors: docList
         });
       });
   }
 
   render() {
+    console.log(this.state)
     return (
       <div>
         <h1>Doctor's Page</h1>
-        <div>
           <DoctorList doctors={this.state.doctors} />
-        </div>
       </div>
     );
   }
