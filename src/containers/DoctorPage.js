3fbaaa30e7881ea7539c42cfcf7052ec43d6807d
mguevara717 <marissa.guevara717@gmail.com>
2018-11-25 10:56:45 -0500

    added main feature components

---
+++ src/containers/DoctorPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class DoctorPage extends React.Component {
+
+render() {
+  return(
+    <div>
+      <h1>Testing from DoctorPage</h1>
+    </div>
+  )
+}
+
+
+
+}
+export default DoctorPage;
