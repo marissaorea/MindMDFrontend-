a390800bb99480cbabdba17080cbe6d1196e14e3
mguevara717 <marissa.guevara717@gmail.com>
2018-11-26 17:11:40 -0500

    rendered google maps API

--- src/containers/GoogleMap.js
+++ src/containers/GoogleMap.js
@@ -1,15 +1,29 @@
-import React from 'react'
-// import "../GoogleMap.css";
+import React from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-const GoogleMap = (props) => {
+class GoogleMap extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <div>
+          <h1>Message from GoogleMap Component</h1>
+        </div>
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
-  console.log(props)
-  return(
-    <div>
-      <h1>Message from GoogleMap Component</h1>
-    </div>
-    )
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>test</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+        ); } }
+      </div>
+    );
+  }
+}
 
-}//end of class
-
-export default GoogleMap;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAUWcQqzKVdF5zFNzSyMxzxJuO4luGC0pk"
+})(GoogleMap)
