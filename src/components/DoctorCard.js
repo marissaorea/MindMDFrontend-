04855a8ca05c5ed16e651382118c0ae5a40700dd
mguevara717 <marissa.guevara717@gmail.com>
2018-11-26 10:41:21 -0500

    minor update

--- src/components/DoctorCard.js
+++ src/components/DoctorCard.js
@@ -3,35 +3,40 @@ import React from "react";
 const DoctorCard = props => {
   console.log(props);
 
-
   return (
     <div className="ui unstackable items">
-  <div className="item">
-    <div className="image">
-      <img src="DocAvatar.png"/>
+      <div className="item">
+        <div className="image">
+          <img src="DocAvatar.png" />
+        </div>
+        <div className="content">
+          <br />
+          <a id="main-header" className="header">
+            Dr. {props.doctorInfo.first_name} <br />
+          </a>
+          <a id="main-header" className="header">
+            {props.doctorInfo.last_name}
+          </a>
+          <div className="meta">
+            <span>{props.doctorInfo.title}</span>
+          </div>
+          <div className="description">
+            <p>Clinic: {props.doctorInfo.office_name}</p>
+            <address>
+              {props.doctorInfo.street}
+              <br />
+              <span>{props.doctorInfo.city}, </span>
+              {props.doctorInfo.street}
+              <br />
+              {props.doctorInfo.zip}
+            </address>
+          </div>
+          <div className="extra">{props.doctorInfo.bio}</div>
+        </div>
+      </div>
     </div>
-    <div className="content">
-      <br />
-      <a id="main-header" className="header">Dr. {props.doctorInfo.first_name} <br /></a>
-      <a id="main-header" className="header">{props.doctorInfo.last_name}</a>
-      <div className="meta">
-        <span>{props.doctorInfo.title}</span>
-      </div>
-      <div className="description">
-      <p>Clinic: {props.doctorInfo.office_name}</p>
-      <address>
-        {props.doctorInfo.street}<br/>
-        <span>{props.doctorInfo.city}, </span>
-        {props.doctorInfo.street}<br/>
-        {props.doctorInfo.zip}
-      </address>
-      </div>
-      <div className="extra">
-        {props.doctorInfo.bio}
-      </div>
-    </div>
-  </div>
-  </div>
   );
 };
+
+
 export default DoctorCard;
