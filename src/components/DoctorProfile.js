b8d0aaa531f5d1d03abf8da86e020b3f5e7a711e
mguevara717 <marissa.guevara717@gmail.com>
2018-11-28 00:02:11 -0500

    improved styling:

---
+++ src/components/DoctorProfile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "semantic-ui-css/semantic.min.css";
+import Popup from "reactjs-popup";
+
+
+const DoctorProfile = () => {
+
+  return(
+    <div>
+    
+    </div>
+  )
+
+
+}
+
+export default DoctorProfile;
