ebec8af16ecc1d2c104420d7bb3583af0adb464c
mguevara717 <marissa.guevara717@gmail.com>
2018-12-05 23:39:37 -0500

    minor update

--- src/components/LoadingContainer.js
+++ src/components/LoadingContainer.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Dimmer, Loader, Image, Segment } from "semantic-ui-react";
 
 const LoadingSpinner = () => {
-  
+
   return (
     <Segment>
       <Dimmer active inverted>
         <Loader inverted content="Loading" />
       </Dimmer>
-
       <Image src="/images/wireframe/short-paragraph.png" />
     </Segment>
   );
